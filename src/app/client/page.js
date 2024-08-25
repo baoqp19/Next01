1fbe46921812703aed44f3c4eb77f788153dafdc
= <=>
2024-08-25 17:03:18 +0700

    Use Router

--- src/app/client/page.js
+++ src/app/client/page.js
@@ -1,10 +1,16 @@
+"use client";
+import { useRouter } from "next/navigation";   // chú ý import
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/admin");
+  };
   return (
-     <main>
-      <h1>
-        Helelo
-      </h1>
-     </main>
+    <main>
+      <h1>Hello</h1>
+      <button onClick={handleClick}>Vào quản trị</button>
+    </main>
   );
 }
